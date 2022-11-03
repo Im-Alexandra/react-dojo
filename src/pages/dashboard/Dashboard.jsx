50c197b6b27facda3be0f27a60ec7a0b0cf2c1ca
Alexandra Labusova <sasa.labusova@gmail.com>
2022-11-03 22:47:24 +0100

    project filters added

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -2,15 +2,59 @@ import React from "react";
 import "./Dashboard.css";
 import { useCollection } from "../../hooks/useCollection";
 import ProjectList from "../../component/ProjectList";
+import ProjectFilter from "./ProjectFilter";
+import { useState } from "react";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 export default function Dashboard() {
+  const [currentFilter, setCurrentFilter] = useState("all");
   const { documents, error } = useCollection("projects");
+  const { user } = useAuthContext();
+
+  const changeFilter = (newFilter) => {
+    setCurrentFilter(newFilter);
+  };
+
+  //this runs every time we change a filter
+  //this is turned into ternary - we first have to check if we have documents and only then do the filtering
+  const filteredProjects = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case "all":
+            return true;
+          case "mine":
+            let assignedToMe = false;
+            document.assignedUsersList.forEach((u) => {
+              if (user.uid === u.id) {
+                assignedToMe = true;
+              }
+            });
+            return assignedToMe;
+          case "development":
+          case "design":
+          case "marketing":
+          case "sales":
+            console.log(document.category, currentFilter);
+            return document.category === currentFilter;
+          default:
+            return true;
+        }
+      })
+    : null;
 
   return (
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
-      {documents && <ProjectList projects={documents} />}
+      {filteredProjects && (
+        <>
+          <ProjectFilter
+            currentFilter={currentFilter}
+            changeFilter={changeFilter}
+          />
+          <ProjectList projects={filteredProjects} />
+        </>
+      )}
     </div>
   );
 }
