8da4366ddaf6544684ba2953e87c7314bd2767d8
Alexandra Labusova <sasa.labusova@gmail.com>
2022-11-03 22:11:45 +0100

    can delete projects when completed

--- src/pages/project/ProjectSummary.jsx
+++ src/pages/project/ProjectSummary.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Avatar from "../../component/Avatar";
+import { useFirestore } from "../../hooks/useFirestore";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useHistory } from "react-router-dom";
 
 export default function ProjectSummary({ project }) {
+  const { deleteDocument, response } = useFirestore("projects");
+  const { user } = useAuthContext();
+  const history = useHistory();
+
+  const handleClick = (e) => {
+    deleteDocument(project.id);
+    history.push("/");
+  };
   return (
     <div>
       <div className="project-summary">
         <h2 className="page-title">{project.name}</h2>
+        <p>By {project.createdBy.displayName}</p>
         <p className="due-data">
           Project due by {project.dueDate.toDate().toDateString()}
         </p>
@@ -19,6 +31,11 @@ export default function ProjectSummary({ project }) {
           ))}
         </div>
       </div>
+      {project.createdBy.id === user.uid && (
+        <button className="btn" onClick={handleClick}>
+          Mark as complete
+        </button>
+      )}
     </div>
   );
 }
